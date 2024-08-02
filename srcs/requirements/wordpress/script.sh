823b7d1c35b1f42dee34c6fb37dbf705932a2e8e
hiono <hiono@hiono.hiono.42.fr>
2024-08-02 16:22:38 +0930

    remove latest tag

--- srcs/requirements/wordpress/script.sh
+++ srcs/requirements/wordpress/script.sh
@@ -8,4 +8,6 @@ chmod +x wp-cli.phar
 ./wp-cli.phar core install --url=localhost --title=$WP_TITLE --admin_user=$WP_ADMIN_USER --admin_password=$WP_ADMIN_PWD --admin_email=$WP_ADMIN_MAIL --skip-email --allow-root
 ./wp-cli.phar user create $WP_USER $WP_MAIL --role=author --user_pass=$WP_PWD --allow-root
 
-php-fpm8.2 -F
+mkdir -p /run/php
+
+/usr/sbin/php-fpm7.3 -F
