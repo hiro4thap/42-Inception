3725f59712a1daada4f64678be6c85611f811012
hiono <hiono@hiono.hiono.42.fr>
2024-08-02 14:46:58 +0930

    add environment variables

--- srcs/requirements/wordpress/script.sh
+++ srcs/requirements/wordpress/script.sh
@@ -4,7 +4,8 @@ cd /var/www/html
 curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
 chmod +x wp-cli.phar
 ./wp-cli.phar core download --allow-root
-./wp-cli.phar config create --dbname=wordpress --dbuser=wpuser --dbpass=password --dbhost=mariadb --allow-root
-./wp-cli.phar core install --url=localhost --title=inception --admin_user=admin --admin_password=admin --admin_email=admin@admin.com --allow-root
+./wp-cli.phar config create --dbname=$db_name --dbuser=$db_admin_user --dbpass=$db_admin_pwd --dbhost=mariadb --allow-root
+./wp-cli.phar core install --url=localhost --title=$WP_TITLE --admin_user=$WP_ADMIN_USER --admin_password=$WP_ADMIN_PWD --admin_email=$WP_ADMIN_MAIL --skip-email --allow-root
+./wp-cli.phar user create $WP_USER $WP_MAIL --role=author --user_pass=$WP_PWD --allow-root
 
 php-fpm8.2 -F
