b5bf841c6f65f29505811c6f928386e31a69d9a6
hiono <hiono@hiono.hiono.42.fr>
2024-08-02 17:02:15 +0930

    modify environment variabls capital

--- srcs/requirements/mariadb/script.sh
+++ srcs/requirements/mariadb/script.sh
@@ -1,17 +1,15 @@
 #!/bin/bash
 
-sed -i 's/DB_ADMIN_USER/'${db_admin_user}'/g' /etc/mysql/init.sql
-sed -i 's/DB_NAME/'${db_name}'/g' /etc/mysql/init.sql
-sed -i 's/DB_ADMIN_PWD/'${db_admin_pwd}'/g' /etc/mysql/init.sql
-sed -i 's/DB_USER/'${db_user}'/g' /etc/mysql/init.sql
-sed -i 's/DB_PWD/'${db_pwd}'/g' /etc/mysql/init.sql
+sed -i 's/DB_ADMIN_USER/'${DB_ADMIN_USER}'/g' /etc/mysql/init.sql
+sed -i 's/DB_NAME/'${DB_NAME}'/g' /etc/mysql/init.sql
+sed -i 's/DB_ADMIN_PWD/'${DB_ADMIN_PWD}'/g' /etc/mysql/init.sql
+sed -i 's/DB_USER/'${DB_USER}'/g' /etc/mysql/init.sql
+sed -i 's/DB_PWD/'${DB_PWD}'/g' /etc/mysql/init.sql
 
-if [ -d "/var/lib/mysql/$db_name" ]; then
+if [ -d "/var/lib/mysql/$DB_NAME" ]; then
   echo "database exists."
   mysqld_safe
 else
   mysql_install_db
   mysqld
 fi
-
-exec "$@"%
--- srcs/requirements/wordpress/script.sh
+++ srcs/requirements/wordpress/script.sh
@@ -4,7 +4,7 @@ cd /var/www/html
 curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
 chmod +x wp-cli.phar
 ./wp-cli.phar core download --allow-root
-./wp-cli.phar config create --dbname=$db_name --dbuser=$db_admin_user --dbpass=$db_admin_pwd --dbhost=mariadb --allow-root
+./wp-cli.phar config create --dbname=$DB_NAME --dbuser=$DB_ADMIN_USER --dbpass=$DB_ADMIN_PWD --dbhost=mariadb --allow-root
 ./wp-cli.phar core install --url=localhost --title=$WP_TITLE --admin_user=$WP_ADMIN_USER --admin_password=$WP_ADMIN_PWD --admin_email=$WP_ADMIN_MAIL --skip-email --allow-root
 ./wp-cli.phar user create $WP_USER $WP_MAIL --role=author --user_pass=$WP_PWD --allow-root
 
